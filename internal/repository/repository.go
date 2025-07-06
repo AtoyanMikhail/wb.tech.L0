20a7b4d3d9d335d6f3cd9c0d5b8e9953554b8b46
AtoyanMikhail <mikhail.atoyan@gmail.com>
2025-07-07 02:28:01 +0300

    fixed repo interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -7,6 +7,7 @@ import (
 
 type OrderRepository interface {
 	SaveOrder(ctx context.Context, order *models.Order) error
+	RunMigrations(migrationsPath string) error
 	GetOrderByID(ctx context.Context, orderUID string) (*models.Order, error)
 	GetAllOrders(ctx context.Context) ([]models.Order, error)
 }
